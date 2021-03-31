6c70fed8c60b333cb3cd26e4d0b2d549932f2cf8
hernan74 <hernanlopez74@gmail.com>
2021-03-31 18:56:42 -0300

    avance

--- lib/src/pages/pagina_1.dart
+++ lib/src/pages/pagina_1.dart
@@ -27,6 +27,16 @@ class Pagina1 extends StatelessWidget {
                   Navigator.push(context, _rotationTransition(Pagina2()));
                 },
                 child: Text('Rotation Transition')),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, _fadeTransition(Pagina2()));
+                },
+                child: Text('Fade Transition')),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, _trancicionConvinada(Pagina2()));
+                },
+                child: Text('Trancicion Convinada')),
           ],
         ),
       ),
@@ -72,6 +82,42 @@ class Pagina1 extends StatelessWidget {
           position: Tween<Offset>(begin: Offset(-0.3, 1.0), end: Offset.zero)
               .animate(curveAnimation),
           child: child,
+        );
+      },
+    );
+  }
+
+  Route _fadeTransition(Widget child) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => child,
+      transitionDuration: Duration(seconds: 1),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        final curveAnimation =
+            CurvedAnimation(parent: animation, curve: Curves.easeInOut);
+
+        return FadeTransition(
+          opacity: Tween<double>(begin: 0.0, end: 1.0).animate(curveAnimation),
+          child: child,
+        );
+      },
+    );
+  }
+
+  Route _trancicionConvinada(Widget child) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => child,
+      transitionDuration: Duration(seconds: 1),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        final curveAnimation =
+            CurvedAnimation(parent: animation, curve: Curves.easeInOut);
+
+        return FadeTransition(
+          opacity: Tween<double>(begin: 0.0, end: 1.0).animate(curveAnimation),
+          child: SlideTransition(
+            position: Tween<Offset>(begin: Offset(-0.3, 1.0), end: Offset.zero)
+                .animate(curveAnimation),
+            child: child,
+          ),
         );
       },
     );
